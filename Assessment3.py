62292d723ece902d991ec8641dd4d93cceee0428
vlyras20 <43887542+vlyras20@users.noreply.github.com>
2019-05-05 19:42:58 +0100

    Commit

--- Assessment3.py
+++ Assessment3.py
@@ -1,85 +1,68 @@
 import math, random, sys
 import pygame
-from pygame.locals import *
 from gameSettings import *
 from gameSprites import *
 
 class Game:
 	def __init__(self):
 		pygame.init()
+		pygame.mixer.init()
 		self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
 		pygame.display.set_caption(TITLE)
-		self.clock = pg.time.Clock()
-		pygame.key.set_repeat(500, 100)
+		self.clock = pygame.time.Clock()
+		self.running = True
+
+	def new(self):
+		self.all_sprites = pygame.sprite.Group()
+		self.player = Player()
+		self.all_sprites.add(self.player)
+		self.run()
 
     # game loop - set self.playing = False to end the game
-    def run(self):
-        self.playing = True
-        while self.playing:
-            self.dt = self.clock.tick(FPS) / 1000
-            self.events()
-            self.update()
-            self.draw()
-
-    def draw(self):
-        self.screen.fill(BGCOLOR)
-        self.draw_grid()
-        self.all_sprites.draw(self.screen)
-        pg.display.flip()
+	def run(self):
+		self.playing = True
+		while self.playing:
+			self.clock.tick(FPS)
+			self.events()
+			self.update()
+			self.draw()
 
     # update portion of the game loop
-    def update(self):
-        self.all_sprites.update()
+	def update(self):
+		self.all_sprites.update()
+		if self.player.rect.top <= HEIGHT / 2:
+			self.player.velocity.y = 0
+			self.player.pos.y += abs(self.player.velocity.y)
 
 	# exit the program
-	def events():
+	def events(self):
 		for event in pygame.event.get():
-			if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-				pygame.quit()
-				sys.exit()
+			if event.type == pygame.QUIT:
+				if self.playing:
+					self.playing = False
+				self.running = False
 
-    def titleScreen(self):
-        pass
+
+	def draw(self):
+		self.screen.fill(SEA)
+		self.screen.blit(seabg,(bgx, 505))
+		self.all_sprites.draw(self.screen)
+		pygame.display.flip()
+
+	def titleScreen(self):
+		pass
 
 g = Game()
 g.titleScreen()
 
+seabg = pygame.image.load('Images/sea.png')
+bgx = 0
+
 # main loop
-while True:
-	g.run()
+while g.running:
+	g.new()
+	g.titleScreen()
 
-	key = pygame.key.get_pressed()
-	if key[K_UP]:
-		playerPosY -= 5
-	if key[K_DOWN]:
-		playerPosY += 5
-	if key[K_RIGHT]:
-		playerVelocityX = 5
-	elif key[K_LEFT]:
-		playerVelocityX = -5
-	else:
-		playerVelocityX = 0
 
-	playerPosX += playerVelocityX
-	if playerPosX > stageWidth - circleRadius:
-		playerPosX = stageWidth - circleRadius
-	if playerPosX < circleRadius:
-		playerPosX = circleRadius
-	if playerPosX < startScrollingPosX:
-		circlePosX = playerPosX
-	elif playerPosX > stageWidth - startScrollingPosX:
-		circlePosX = playerPosX - stageWidth + W
-	else:
-		circlePosX = startScrollingPosX
-		stagePosX += -playerVelocityX
-
-	rel_x = stagePosX % bgWidth
-	DS.blit(bg, (rel_x - bgWidth, 0))
-	if rel_x < W:
-		DS.blit(bg, (rel_x, 0))
-
-	pygame.draw.circle(DS, WHITE, (int(circlePosX), playerPosY - circleRadius), circleRadius, 0)
-
-	pygame.display.update()
-	CLOCK.tick(FPS)
-	DS.fill(BLACK)
+pygame.quit()
+	# pygame.draw.circle(DS, WHITE, (int(circlePosX), playerPosY - circleRadius), circleRadius, 0)
--- gameSprites.py
+++ gameSprites.py
@@ -1,23 +1,49 @@
-import pygame as pg
-from settings import *
+import pygame
+from gameSettings import *
 
-class Player(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
-        self.groups = game.all_sprites
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(YELLOW)
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('Images/boatIdleLeft.png')
         self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-
-
-    def move(self, dx=0, dy=0):
-        self.x += dx
-        self.y += dy
-
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = pygame.math.Vector2(WIDTH/2, HEIGHT/1.2)
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.acceleration = pygame.math.Vector2(0, 0)
 
     def update(self):
-        self.rect.x = self.x * TILESIZE
-        self.rect.y = self.y * TILESIZE
+        self.acceleration = pygame.math.Vector2(0, 0)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.acceleration.x = -0.5
+            self.image = pygame.image.load('Images/boatMoveLeft.png')
+            playerVelocityX = -1
+        elif keys[pygame.K_RIGHT]:
+            self.acceleration.x = 0.5
+            self.image = pygame.image.load('Images/boatMoveRight.png')
+            playerVelocityX = 1
+        else:
+            self.image = pygame.image.load('Images/boatIdleLeft.png')
+            playerVelocityX = 0
+
+        self.acceleration += self.velocity * -0.09
+        self.velocity += self.acceleration
+        self.pos += self.velocity + 0.5 * self.acceleration
+        self.pos.x = playerPosX
+        self.velocity.x = playerVelocityX
+        # if self.pos.x > 455:
+        #     self.pos.x = 455
+        # elif self.pos.x < 30:
+        #     self.pos.x = 30
+
+        # self.pos.y -= currentYvelocity
+        self.rect.center = self.pos
+
+# class Sea(pygame.sprite.Sprite):
+#     def __init__(self):
+#         pygame.sprite.Sprite.__init__(self)
+#         self.image = pygame.image.load('Images/sea.png')
+#         # self.image.fill(YELLOW)
+#         self.rect = self.image.get_rect()
+#         self.rect.center = (WIDTH/2, HEIGHT/2)
+#         self.pos = pygame.math.Vector2(WIDTH/2, HEIGHT/2)
